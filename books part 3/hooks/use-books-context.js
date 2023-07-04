b4301cd06d68bd97abc8fea45c3f5f5bf3093e62
Zhen Hong <60844852+zhenhong1001@users.noreply.github.com>
2023-07-04 22:50:52 +0800

    Delete books part 3 directory

--- books part 3/hooks/use-books-context.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import BooksContext from "../context/books";
-
-function useBooksContext(){
-    return useContext(BooksContext);
-}
-
-export default useBooksContext;
