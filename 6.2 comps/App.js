0fcdf823cd1310c9b3cda28cd7200aca924c413a
Zhen Hong <60844852+zhenhong1001@users.noreply.github.com>
2023-09-12 15:46:45 +0800

    Add files via upload
    practice props and state design

---
+++ 6.2 comps/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import Dropdown from './components/Dropdown'
+
+function App() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    {label: 'Red', value:'red'},
+    {label: 'Green', value:'green'},
+    {label: 'Blue', value:'blue'},
+  ]
+
+  return (
+    <div className='flex'>
+      <Dropdown options={options} value={selection} onChange={handleSelect}/>
+      <Dropdown options={options} value={selection} onChange={handleSelect}/>
+    </div>);
+
+}
+
+export default App;
